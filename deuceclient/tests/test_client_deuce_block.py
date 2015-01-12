7891d95ac2aefcff23c280887f3a223c0e6dcac2
BenjamenMeyer <ben.meyer@rackspace.com>
2015-01-12 22:40:25 +0000

    Enhancement: Update Deuce-Client for Deuce PR #247
    - Deuce PR #247 allows us to be able to track from the client-side which block id maps to which storage id when using the msgpack block upload interface. This provides the requisite changes to work with Deuce with those changes applied

--- deuceclient/tests/test_client_deuce_block.py
+++ deuceclient/tests/test_client_deuce_block.py
@@ -143,6 +143,7 @@ class ClientDeuceBlockTests(ClientTestBase):
 
     def test_blocks_upload(self):
         blocks = []
+        response_data = {}
         for block_id, blockdata, block_size in [create_block()
                                                 for _ in range(5)]:
             blocks.append(block_id)
@@ -151,11 +152,13 @@ class ClientDeuceBlockTests(ClientTestBase):
                                 block_id=block_id,
                                 data=blockdata)
             self.vault.blocks[block_id] = a_block
+            response_data[block_id] = create_storage_block(block_id)
 
         httpretty.register_uri(httpretty.POST,
                                get_blocks_url(self.apihost,
                                               self.vault.vault_id),
-                               status=201)
+                               status=200,
+                               body=json.dumps(response_data))
 
         self.assertTrue(self.client.UploadBlocks(self.vault, blocks))
 
